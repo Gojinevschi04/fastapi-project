e6e9636ff6379470724d5e565fdddb894e7711a6
Gojinevschi04 <annagojinevschi@gmail.com>
2026-02-20 10:16:51 +0200

    refactor: update main application entry point

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.modules.files.views import router as files_router
 from app.modules.users.views import router as auth_router
 
-app = FastAPI()
+
+def get_application() -> FastAPI:
+    app = FastAPI(title="FastAPIProject", version="1.0.0")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(files_router)
+    app.include_router(auth_router)
+
+    return app
 
 
-app.include_router(files_router)
-app.include_router(auth_router)
+app = get_application()
